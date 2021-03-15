e0b5c5b6d94d0603ce397d74826ee583c17b6be9
IvoLims <75138193+IvoLims@users.noreply.github.com>
2021-03-15 11:43:16 +0000

    Update generator.cpp

--- Src/generator.cpp
+++ Src/generator.cpp
@@ -14,33 +14,24 @@ using namespace std;
 string gen_Plane(float width){
     width /= 2;
     stringstream ss;
-    ss << ":Plane\n";
 
     ss << width << " " << 0 << " " << width << "\n";
     ss << width << " " << 0 << " " << -width << "\n";
     ss << -width << " " << 0 << " " << -width << "\n";
 
-    ss << "-------------------------------------\n";
-
     ss << width << " " << 0 << " " << width << "\n";
     ss << -width << " " << 0 << " " << -width << "\n";
     ss << -width << " " << 0 << " " << width << "\n";
-
-    ss << "-------------------------------------\n";
 
     ss << width << " " << 0 << " " << -width << "\n";
     ss << width << " " << 0 << " " << width << "\n";
     ss << -width << " " << 0 << " " << -width << "\n";
 
-    ss << "-------------------------------------\n";
-
     ss << -width << " " << 0 << " " << -width << "\n";
     ss << width << " " << 0 << " " << width << "\n";
     ss << -width << " " << 0 << " " << width << "\n";
 
-    ss << "-------------------------------------\n";
-
-    return "4" + ss.str();
+    return "4\n" + ss.str();
 }
 
 string gen_Box(float x,float y,float z){
@@ -48,83 +39,56 @@ string gen_Box(float x,float y,float z){
     y /= 2;
     z /= 2;
     stringstream ss;
-    ss << ":Box\n";
-
-    ss << "--Top--\n";
 
     ss << x << " " << y << " " << z << "\n";
     ss << x << " " << y << " " << -z << "\n";
     ss << -x << " " << y << " " << -z << "\n";
 
-    ss << "-------------------------------------\n";
-
     ss << x << " " << y << " " << z << "\n";
     ss << -x << " " << y << " " << -z << "\n";
     ss << -x << " " << y << " " << z << "\n";
-
-    ss << "--Bottom--\n";
 
     ss << x << " " << -y << " " << -z << "\n";
     ss << x << " " << -y << " " << z << "\n";
     ss << -x << " " << -y << " " << -z << "\n";
 
-    ss << "-------------------------------------\n";
-
     ss << -x << " " << -y << " " << -z << "\n";
     ss << x << " " << -y << " " << z << "\n";
     ss << -x << " " << -y << " " << z << "\n";
-
-    ss << "--Right--\n";
 
     ss << x << " " << -y << " " << -z << "\n";
     ss << x << " " << y << " " << -z << "\n";
     ss << x << " " << y << " " << z << "\n";
 
-    ss << "-------------------------------------\n";
-
     ss << x << " " << -y << " " << -z << "\n";
     ss << x << " " << y << " " << z << "\n";
     ss << x << " " << -y << " " << z << "\n";
-
-    ss << "--Left--\n";
 
     ss << -x << " " << y << " " << -z << "\n";
     ss << -x << " " << -y << " " << -z << "\n";
     ss << -x << " " << y << " " << z << "\n";
 
-    ss << "-------------------------------------\n";
-
     ss << -x << " " << y << " " << z << "\n";
     ss << -x << " " << -y << " " << -z << "\n";
     ss << -x << " " << -y << " " << z << "\n";
-
-    ss << "--Front--\n";
 
     ss << x << " " << -y << " " << z << "\n";
     ss << x << " " << y << " " << z << "\n";
     ss << -x << " " << y << " " << z << "\n";
 
-    ss << "-------------------------------------\n";
-
     ss << x << " " << -y << " " << z << "\n";
     ss << -x << " " << y << " " << z << "\n";
     ss << -x << " " << -y << " " << z << "\n";
-
-    ss << "--Rear--\n";
 
     ss << x << " " << y << " " << -z << "\n";
     ss << x << " " << -y << " " << -z << "\n";
     ss << -x << " " << y << " " << -z << "\n";
 
-    ss << "-------------------------------------\n";
-
     ss << -x << " " << y << " " << -z << "\n";
     ss << x << " " << -y << " " << -z << "\n";
     ss << -x << " " << -y << " " << -z << "\n";
 
-    ss << "-------------------------------------\n";
-
-    return "12" + ss.str();
+    return "12\n" + ss.str();
 
 }
 
@@ -132,10 +96,8 @@ string gen_Sphere(float radius,int slices,int stacks){
     float a_interval = 2 * PI / slices;
     float b_interval = PI / stacks;
     float next_a, next_b;
-
     int triangles = 0;
     stringstream ss;
-    ss << ":Sphere\n";
 
     for (float a = 0; a < 2 * PI; a +=  a_interval) {
         for (float b = -PI/2; b < PI/2; b += b_interval) {
@@ -151,35 +113,29 @@ string gen_Sphere(float radius,int slices,int stacks){
             ss << radius * cos(next_b) * sin(a) << " " << radius * sin(next_b) << " " << radius * cos(next_b) * cos(a) << "\n";
             ss << radius * cos(b) * sin(next_a) << " " << radius * sin(b) << " " << radius * cos(b) * cos(next_a) << "\n";
 
-            ss << "-------------------------------------\n";
-
             ss << radius * cos(b) * sin(next_a) << " " << radius * sin(b) << " " << radius * cos(b) * cos(next_a) << "\n";
             ss << radius * cos(next_b) * sin(a) << " " << radius * sin(next_b) << " " << radius * cos(next_b) * cos(a) << "\n";
             ss << radius * cos(b) * sin(a) << " " << radius * sin(b) << " " << radius * cos(b) * cos(a) << "\n";
 
-            ss << "-------------------------------------\n";
             triangles += 2;
         }
     }
-    return to_string(triangles) + ss.str();
+    return to_string(triangles) + "\n" + ss.str();
 }
 
 string gen_Cone(float radius,float height,int slices,int stacks){
-    float interval = 2 * PI / slices;
     float next_a;
     float next_h;
+    float interval = 2 * PI / slices;
     float stack_height = height / stacks;
-
     int triangles = 0;
     stringstream ss;
-    ss << ":Cone\n";
 
     for (float a = 0; a < 2 * PI; a += interval) {
         next_a = a + interval;
         if (next_a > 2 * PI) {
             next_a = 2 * PI;
         }
-        ss << "--Bottom--\n";
         ss << 0.0f << " " << 0 << " " << 0.0f << "\n";
         ss << radius * sin(next_a) << " " << 0 << " " << radius * cos(next_a) << "\n";
         ss << radius * sin(a) << " " << 0 << " " << radius * cos(a) << "\n";
@@ -191,23 +147,19 @@ string gen_Cone(float radius,float height,int slices,int stacks){
             if (next_h > height) {
                 next_h = height;
             }
-            ss << "--Walls--\n";
             ss << radius * sin(next_a) * ((height - next_h) / height) << " " << next_h << " " << radius * cos(next_a) * ((height - next_h) / height) << "\n";
             ss << radius * sin(a) * ((height - next_h) / height) << " " << next_h << " " << radius * cos(a) * ((height - next_h) / height) << "\n";
             ss << radius * sin(next_a) * ((height - h) / height) << " " << h << " " << radius * cos(next_a) * ((height - h) / height) << "\n";
-
-            ss << "-------------------------------------\n";
 
             ss << radius * sin(a) * ((height - next_h) / height) << " " << next_h << " " << radius * cos(a) * ((height - next_h) / height) << "\n";
             ss << radius * sin(a) * ((height - h) / height) << " " << h << " " << radius * cos(a) * ((height - h) / height) << "\n";
             ss << radius * sin(next_a) * ((height - h) / height) << " " << h << " " << radius * cos(next_a) * ((height - h) / height) << "\n";
 
-            ss << "-------------------------------------\n";
             triangles += 2;
         }
     }
 
-    return to_string(triangles) + ss.str();;
+    return to_string(triangles) + "\n" + ss.str();;
 }
 
 /*-------------------Construction file function-------------------*/
