1c5f1804709c889d4964d0861d4ef089c8ea3798
IvoLims <75138193+IvoLims@users.noreply.github.com>
2021-03-23 15:51:22 +0000

    Update engine.cpp

--- Fase_2/Source/code/engine.cpp
+++ Fase_2/Source/code/engine.cpp
@@ -1,5 +1,8 @@
-#include <math.h>
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <vector>
+#include <ctime>
+
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -15,13 +18,13 @@
 #include <cstdio>
 #include "tinyxml2.h"
 #include "tinyxml2.cpp"
+
 using namespace std;
 using namespace tinyxml2;
 
-#define PI 3.1415926535897932384626433832795
 #define INIT_R 7
-#define INIT_ALPHA PI/4
-#define INIT_BETA PI/4
+#define INIT_ALPHA M_PI/4
+#define INIT_BETA M_PI/4
 #define ALPHA_JUMP 0.05
 #define BETA_JUMP 0.04
 
@@ -29,6 +32,13 @@ float mouse_x_init, mouse_y_init;
 float mouse_x_prev, mouse_y_prev;
 
 float px, py, pz, alpha, beta, r;
+
+//For Window
+int window;
+
+//For FPS counter
+int timebase;
+float frame;
 
 GLuint vertices, verticeCount;
 
@@ -58,83 +68,42 @@ void changeSize(int w, int h) {
 	glMatrixMode(GL_MODELVIEW);
 }
 
-// write function to process keyboard events
-void mouse(int button, int state, int mouse_x, int mouse_y) {
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-	{
-		mouse_x_init = mouse_x;
-		mouse_y_init = mouse_y;
-		mouse_x_prev = mouse_x_init;
-		mouse_y_prev = mouse_y_init;
-		//glutPostRedisplay();
-	}
+void drawAxis() {
+	glBegin(GL_LINES);
+	// X axis in Red
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(100.0f, 0.0f, 0.0f);
+	glVertex3f(-100.0f, 0.0f, 0.0f);
+	// Y Axis in Green
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 100.0f, 0.0f);
+	glVertex3f(0.0f, -100.0f, 0.0f);
+	// Z Axis in Blue
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 100.0f);
+	glVertex3f(0.0f, 0.0f, -100.0f);
+	glEnd();
 }
-
-void mouse_motion(int mouse_x, int mouse_y) {
-	if (mouse_x > mouse_x_prev) {
-		alpha -= ALPHA_JUMP;
-	}
-	if (mouse_x < mouse_x_prev) {
-		alpha += ALPHA_JUMP;
-	}
-	if (mouse_y > mouse_y_prev) {
-		if (beta < PI / 2 - BETA_JUMP) {
-			beta += BETA_JUMP;
-		}
-	}
-	if (mouse_y < mouse_y_prev) {
-		if (beta > -PI / 2 + BETA_JUMP) {
-			beta -= BETA_JUMP;
-		}
-	}
-
-	mouse_x_prev = mouse_x;
-	mouse_y_prev = mouse_y;
-	glutPostRedisplay();
-
-}
-
-void keyboard_func(unsigned char c, int mouse_x, int mouse_y) {
-	if (c == ' ') {
-		alpha = INIT_ALPHA;
-		beta = INIT_BETA;
-		r = INIT_R;
-	}
-	else if (c == 'a') {
-	}
-	else if (c == 'd') {
-	}
-	else if (c == 'w') {
-		r -= 0.2;
-	}
-	else if (c == 's') {
-		r += 0.2;
-	}
-	else if (c == 'q') {
-	}
-	else if (c == 'e') {
-	}
-	else if (c == '1') {
-	}
-	else if (c == '2') {
-	}
-
-	glutPostRedisplay();
-}
-
 
 void draw() {	
 	XMLDocument doc;
-	doc.LoadFile( "xmlconf.xml" );
-	const char* title = doc.FirstChildElement( "scene" )->FirstChildElement("model")->Attribute("file");
+	XMLError load = doc.LoadFile( "xmlconf.xml" );
+        if (load != XML_SUCCESS){
+	        cout << "Error in XML file\n"<<endl;
+		return;
+        }
+	XMLNode* pRoot = doc.FirstChildElement("scene");
+	if (pRoot == nullptr) return;
+
 	string ch;
 	ifstream file;
 	vector<float> v;
 	int size;
+	const char* title = doc.FirstChildElement("scene")->FirstChildElement("model")->Attribute("file");
 
 	file.open(title, ios::in);
 	if (!file) {
-		cout << "No such file";
+		cout << "No such file" << endl;
 		return;
 	}
 	file >> ch;
@@ -151,10 +120,10 @@ void draw() {
 	glGenBuffers(1, &vertices);
 	glBindBuffer(GL_ARRAY_BUFFER, vertices);
 	glBufferData(
-		GL_ARRAY_BUFFER, //tipo do buffer, só é relevante na altura do desenho
+		GL_ARRAY_BUFFER, //tipo do buffer, sÃ³ Ã© relevante na altura do desenho
 		sizeof(float) * v.size(), //tamanho do vector em bytes
 		v.data(), //os dados do array associado ao vector
-		GL_STATIC_DRAW); //indicativo da utilização (estático e para desenho)
+		GL_STATIC_DRAW); //indicativo da utilizaÃ§Ã£o (estÃ¡tico e para desenho)
 }
 
 
@@ -170,26 +139,91 @@ void renderScene(void) {
 	// put the geometric transformations here
 
 	// put drawing instructions here
-	glBegin(GL_LINES);
-	// X axis in Red
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(100.0f, 0.0f, 0.0f);
-	glVertex3f(-100.0f, 0.0f, 0.0f);
-	// Y Axis in Green
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(0.0f, 100.0f, 0.0f);
-	glVertex3f(0.0f, -100.0f, 0.0f);
-	// Z Axis in Blue
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(0.0f, 0.0f, 100.0f);
-	glVertex3f(0.0f, 0.0f, -100.0f);
-	glEnd();
-	
+
+	drawAxis();
+		
 	glBindBuffer(GL_ARRAY_BUFFER, vertices);
 	glVertexPointer(3, GL_FLOAT, 0, 0);
 	glDrawArrays(GL_TRIANGLES, 0, verticeCount);
+
+	//FPS counter
+	frame++;
+	int final_time = glutGet(GLUT_ELAPSED_TIME);
+
+	if (final_time - timebase > 1000) {
+		int fps = frame * 1000.0f / (final_time - timebase);
+		char title[(((sizeof fps) * CHAR_BIT) + 2) / 3 + 2];
+		sprintf(title, "FPS: %d", fps);
+		glutSetWindowTitle(title);
+		timebase = final_time;
+		frame = 0;
+	}
+
 	// End of frame
 	glutSwapBuffers();
+}
+
+// write function to process keyboard events
+void mouse(int button, int state, int mouse_x, int mouse_y) {
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		mouse_x_init = mouse_x;
+		mouse_y_init = mouse_y;
+		mouse_x_prev = mouse_x_init;
+		mouse_y_prev = mouse_y_init;
+	}
+}
+
+void mouse_motion(int mouse_x, int mouse_y) {
+	if (mouse_x > mouse_x_prev) {
+		alpha -= ALPHA_JUMP;
+	}
+	if (mouse_x < mouse_x_prev) {
+		alpha += ALPHA_JUMP;
+	}
+	if (mouse_y > mouse_y_prev) {
+		if (beta < M_PI / 2 - BETA_JUMP) {
+			beta += BETA_JUMP;
+		}
+	}
+	if (mouse_y < mouse_y_prev) {
+		if (beta > -M_PI / 2 + BETA_JUMP) {
+			beta -= BETA_JUMP;
+		}
+	}
+
+	mouse_x_prev = mouse_x;
+	mouse_y_prev = mouse_y;
+	glutPostRedisplay();
+
+}
+
+void keyboard_func(unsigned char c, int mouse_x, int mouse_y) {
+
+	switch (c) {
+	case 27: {                    //this is escape key
+		glutDestroyWindow(window);
+		exit(0);
+	}
+	case ' ': {
+		alpha = INIT_ALPHA;
+		beta = INIT_BETA;
+		r = INIT_R;
+		break;
+	}
+	case 'w': {
+		r -= 0.2;
+		break;
+	}
+	case 's': {
+		r += 0.2;
+		break;
+	}
+	default: {
+		break;
+	}
+}
+	glutPostRedisplay();
 }
 
 int main(int argc, char** argv) {
@@ -203,13 +237,17 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(800, 800);
-	glutCreateWindow("Window");
+	window = glutCreateWindow("Window");
 
 	glewInit();
 	draw();
 	// Required callback registry 
+	glutIdleFunc(renderScene);
 	glutDisplayFunc(renderScene);
 	glutReshapeFunc(changeSize);
+
+	//FPS Counter
+	timebase = glutGet(GLUT_ELAPSED_TIME);
 
 	// put here the registration of the keyboard callbacks
 	glutMouseFunc(mouse);
